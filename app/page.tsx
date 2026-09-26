46cbef698771d85b8e6aaf02d7785397076e13cc
mandellasly1 <akpomah_nelson@yahoo.com>
2026-09-26 10:37:48 +0100

    Fix: resolved merge conflicts in layout.tsx, page.tsx, package.json, and package-lock.json

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import Image from "next/image";
-=======
-import Image from 'next/image';
->>>>>>> d0bdbb524b0da001ad3b175e504b7b4d4c073e04
 
 export default function Home() {
   return (
@@ -18,7 +14,6 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-<<<<<<< HEAD
             To get started, edit the{" "}
             <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
               page.tsx
@@ -27,38 +22,19 @@ export default function Home() {
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
-=======
-            To get started, edit the{' '}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{' '}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{' '}
->>>>>>> d0bdbb524b0da001ad3b175e504b7b4d4c073e04
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
               Templates
-<<<<<<< HEAD
             </a>{" "}
             or the{" "}
-=======
-            </a>{' '}
-            or the{' '}
->>>>>>> d0bdbb524b0da001ad3b175e504b7b4d4c073e04
             <a
               href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
               Learning
-<<<<<<< HEAD
             </a>{" "}
-=======
-            </a>{' '}
->>>>>>> d0bdbb524b0da001ad3b175e504b7b4d4c073e04
             center.
           </p>
         </div>
