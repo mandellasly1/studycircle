754c533caa86012661d331c0fb0c0aa5d2f2fb42
mandellasly1 <akpomah_nelson@yahoo.com>
2026-09-26 10:07:41 +0100

    Fix: resolved merge conflict in postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,10 +1,8 @@
+/** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-<<<<<<< HEAD
-    "@tailwindcss/postcss": {},
-=======
-    '@tailwindcss/postcss': {},
->>>>>>> d0bdbb524b0da001ad3b175e504b7b4d4c073e04
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
 
